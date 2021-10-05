99f2efc7efaa4899ac2e927770d72c40b52bba55
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2021-10-05 17:52:29 +0100

    .....

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ import { StyleSheet, View } from "react-native";
 
 // import BasicChart from "./components/basic";
 // import CategoriesChart from './components/categories';
-import DynamicChart from './components/dynamic';
-// import LinearGradientBackground from "./components/background";
+// import DynamicChart from './components/dynamic';
+import LinearGradientBackground from "./components/background";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <BasicChart /> */}
       {/* <CategoriesChart /> */}
-      <DynamicChart />
-      {/* <LinearGradientBackground /> */}
+      {/* <DynamicChart /> */}
+      <LinearGradientBackground />
     </View>
   );
 }
--- components/background.js
+++ components/background.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions, StatusBar } from "react-native";
 import Svg, { Defs, LinearGradient, Stop, Rect } from "react-native-svg";
 
 const LinearGradientBackground = () => {
-  const height = 300;
-  const width = 300;
+  const height = Dimensions.get("screen").height;
+  const width = Dimensions.get("screen").width;
   return (
-    <Svg height={height} width={width} style={styles.container}>
-      <Defs>
-        <LinearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
-          <Stop offset="0" stopColor={"#f3e7e9"} stopOpacity="1" />
-          <Stop offset="1" stopColor={"#e3eeff"} stopOpacity="0.6" />
-        </LinearGradient>
-      </Defs>
-      <Rect height={height} width={width} fill="url(#grad)" />
-    </Svg>
+    <>
+      <StatusBar barStyle="light-content" />
+      <Svg height={height} width={width} style={styles.container}>
+        <Defs>
+          <LinearGradient id="grad" x1="0" y1="0" x2="0" y2="1">
+            <Stop offset="0" stopColor="#96fbc4" stopOpacity="1" />
+            <Stop offset="1" stopColor="#f9f586" stopOpacity="0.6" />
+          </LinearGradient>
+        </Defs>
+        <Rect height={height} width={width} fill="url(#grad)" />
+      </Svg>
+    </>
   );
 };
 
@@ -22,7 +25,7 @@ export default LinearGradientBackground;
 
 const styles = StyleSheet.create({
   container: {
-      zIndex: -1,
+    zIndex: -1,
     position: "absolute",
   },
 });
